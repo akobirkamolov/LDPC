651fc4882d6b0507be1e98b80f2decdd11766a9c
Akobir Kamolov <akobirinfinity@gmail.com>
2024-06-28 16:48:25 +0900

    Create bp_ldpc_decode.py
    Python implementation of 1 log-likelihood belief propagation decoding algorithm for Binary LDPC Codes [649-650, Todd K.Moon, Error Correction Coding, 2005]

---
+++ bp_ldpc_decode.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def ldpc_decode(H, Ln, max_iter):
+    N = H.shape[1]  # number of variable nodes
+    M = H.shape[0]  # number of check nodes
+    
+    # Initialize messages
+    L_nm = np.tile(Ln, (M, 1))
+    
+    for iter in range(max_iter):
+        # Check Node to Variable Node Step
+        for m in range(M):
+            for n in np.where(H[m, :])[0]:
+                prod = np.prod(np.tanh(L_nm[m, H[m, :] == 1] / 2))
+                L_mn[m, n] = 2 * np.arctanh(prod / np.tanh(L_nm[m, n] / 2))
+        
+        # Variable Node to Check Node Step
+            for m in np.where(H[:, n])[0]:
+                L_nm[m, n] = Ln[n] + np.sum(L_mn[H[:, n] == 1, n]) - L_mn[m, n]
+        
+        # Compute output likelihoods
+        Ln_out = Ln + np.sum(L_mn[:, :], axis=0)
+        
+        # Make decisions
+        c_hat = (Ln_out < 0).astype(int)
+        
+        # Check parity
+        if np.all(np.dot(H, c_hat) % 2 == 0):
+            return c_hat, "Success"
+    
+    # If max iterations reached without convergence
+    return c_hat, "Fail"
